8dd54525c197b7bb6947b2144d8c9cab81dfb3b4
Tarek Adel <59666734+tarek-code@users.noreply.github.com>
2025-10-06 13:23:13 +0300

    Delete konecta-task9-gcp-project-id-3 directory

--- konecta-task9-gcp-project-id-3/Modules/route/main.tf
+++
@@ -1,8 +0,0 @@
-resource "google_compute_route" "this" {
-  name             = var.name
-  network          = var.vpc_self_link
-  dest_range       = var.dest_range
-  next_hop_gateway = var.next_hop_gateway
-  priority         = var.priority
-  project          = var.project_id
-}
